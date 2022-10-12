e03d6766c67a52291d3f3a51c554ff58bfacae08
changsubkwak <paradise1732@gmail.com>
2022-10-13 08:37:56 +0900

    refactor: remove main method

--- src/main/java/org/example/MoneyChangingMachine.java
+++ src/main/java/org/example/MoneyChangingMachine.java
@@ -22,12 +22,4 @@ public class MoneyChangingMachine {
         return Integer.toHexString(won);
     }
 
-    public static void main(String[] args) {
-        MoneyChangingMachine moneyChangingMachine = new MoneyChangingMachine();
-        System.out.println("moneyChangingMachine.exchangeWonToDollar(1000) = " + moneyChangingMachine.exchangeWonToDollar(1000));
-        System.out.println("moneyChangingMachine.exchangeWonToDollar(1000) = " + moneyChangingMachine.exchangeWonToYen(1000));
-        System.out.println("moneyChangingMachine.exchangeWonToDollar(1000) = " + moneyChangingMachine.exchangeWonToYuan(1000));
-        System.out.println("moneyChangingMachine.exchangeWonToDollar(1000) = " + moneyChangingMachine.exchangeWonToBinary(1000));
-        System.out.println("moneyChangingMachine.exchangeWonToDollar(1000) = " + moneyChangingMachine.exchangeWonToHex(1000));
-    }
 }
